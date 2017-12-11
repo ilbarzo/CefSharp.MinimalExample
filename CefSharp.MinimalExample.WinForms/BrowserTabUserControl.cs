1029c87c7eb64bfee524b308fbef3e0cd94e7646
Cristian Barzaghi <gbarzo@gmail.com>
2017-12-11 12:06:11 +0100

    Test win form User Control

---
+++ CefSharp.MinimalExample.WinForms/BrowserTabUserControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CefSharp.MinimalExample.WinForms
+{
+    public partial class BrowserTabUserControl : UserControl
+    {
+        public BrowserTabUserControl()
+        {
+            new BrowserTabUserControl("http://www.google.com");
+        }
+        public BrowserTabUserControl(string url)
+        {
+            InitializeComponent();
+
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            var browser = new ChromiumWebBrowser(url)
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(browser);
+        }
+    }
+}
